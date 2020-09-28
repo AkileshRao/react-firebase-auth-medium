a111c5e043e0d02f8bd36fa806f6015c555e8d52
akileshrao <akileshrao19@gmail.com>
2020-09-28 16:06:29 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import PrivateRoute from './helper/PrivateRoute';
-
 import Auth from './components/auth/Auth';
 import Main from './components/main/Main';
-
 import './App.css';
 
 function App() {
@@ -13,8 +10,8 @@ function App() {
       <Router>
         <Switch>
           <Route path='/auth' component={Auth} />
-          <PrivateRoute path='/main' component={Main} />
-          <Redirect to='/auth' from='/' />
+          <Route path='/main' component={Main} />
+          <Redirect to='/auth' from='*' />
         </Switch>
       </Router>
     </div>
